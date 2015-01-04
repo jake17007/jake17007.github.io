7c06076b4b48ac8d43b7e5c603ba997703511f3f
Jake Beauchamp <jake_c_b@comcast.net>
2015-01-03 19:36:02 -0600

    added instagram photos to about me, added github link with button in tech section, changed font in nav

--- Javascript/shrink.js
+++ Javascript/shrink.js
@@ -5,13 +5,16 @@ function init() {
     var distanceY = window.pageYOffset || document.documentElement.scrollTop,
     shrinkOn = 15,
     img = document.querySelector("#sigImg");
+    background = document.querySelector("#nav-background");
     //img = document.getElementById("sigImg");
 
     if (distanceY > shrinkOn) {
       classie.add(img,"smaller");
+      classie.add(background, "smaller");
     } else {
       if (classie.has(img,"smaller")) {
         classie.remove(img,"smaller");
+        classie.remove(background, "smaller");
       }
     }
   });
