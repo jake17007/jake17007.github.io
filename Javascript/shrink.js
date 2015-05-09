e4be1e9ea27e1d9d935105fa2a9cb22517b54073
Jake Beauchamp <jake_c_b@comcast.net>
2015-05-09 11:30:33 -0500

    added links to insta pics, fixed mobile issues: added social

--- Javascript/shrink.js
+++ Javascript/shrink.js
@@ -3,22 +3,23 @@ function init() {
   window.addEventListener('scroll', function(e){
 
     var distanceY = window.pageYOffset || document.documentElement.scrollTop,
-    shrinkOn = 15,
+    shrinkOnSig = 15,
+    shrinkOnSocial = 1,
     img = document.querySelector("#sigImg");
     background = document.querySelector("#nav-background");
+    social = document.querySelector("#socialOnMobile");
     //img = document.getElementById("sigImg");
 
-    if (distanceY > shrinkOn) {
+    if (distanceY > shrinkOnSig) {
       classie.add(img,"smaller");
       classie.add(background, "smaller");
     } else {
       if (classie.has(img,"smaller")) {
         classie.remove(img,"smaller");
         classie.remove(background, "smaller");
-
-
       }
     }
+
   });
 }
 
